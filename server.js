efd84891500b6acc26720b6df08a181321d0fe6b
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-10-04 10:13:31 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -122,4 +122,8 @@ app.get("/", (req, res) => {
   res.render("index", { user: req.user });
 });
 
-app.listen(5000, () => console.log("🚀 Çalışıyor: http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
